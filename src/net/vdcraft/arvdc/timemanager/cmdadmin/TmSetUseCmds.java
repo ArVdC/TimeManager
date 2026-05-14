66de87b07d121a2963374afa7b291e66813c25a4
ArVdC <arnaud.vandenclooster@gmail.com>
2026-05-14 22:50:14 +0200

    Fixed the "/tm set useCmds" command

--- src/net/vdcraft/arvdc/timemanager/cmdadmin/TmSetUseCmds.java
+++ src/net/vdcraft/arvdc/timemanager/cmdadmin/TmSetUseCmds.java
@@ -3,6 +3,7 @@ package net.vdcraft.arvdc.timemanager.cmdadmin;
 import org.bukkit.command.CommandSender;
 
 import net.vdcraft.arvdc.timemanager.MainTM;
+import net.vdcraft.arvdc.timemanager.mainclass.CmdsFileHandler;
 import net.vdcraft.arvdc.timemanager.mainclass.MsgHandler;
 
 public class TmSetUseCmds extends MainTM {
@@ -14,16 +15,14 @@ public class TmSetUseCmds extends MainTM {
 
 		// Check if the argument matches what is expected
 		if (onOff.equalsIgnoreCase(ARG_TRUE) || onOff.equalsIgnoreCase(ARG_FALSE)) {
-			MainTM.getInstance().getConfig().set(CMD_SET_USECMDS, onOff);
-			MainTM.getInstance().saveConfig();
+			MainTM.getInstance().cmdsConf.set(CMD_SET_USECMDS, onOff);
+			CmdsFileHandler.SaveCmdsYml();
 			if (onOff.equalsIgnoreCase(ARG_TRUE)) {
-				debugMode = true;
 				MsgHandler.colorMsg(enableCmdsSchedulerDebugMsg); // Console debug msg (always)
 				MsgHandler.playerAdminMsg(sender, enableCmdsSchedulerDebugMsg); // Player debug msg (in case)
 			} else if (onOff.equalsIgnoreCase(ARG_FALSE)) {
 				MsgHandler.colorMsg(disableCmdsSchedulerDebugMsg); // Console debug msg (always)
 				MsgHandler.playerAdminMsg(sender, disableCmdsSchedulerDebugMsg); // Player debug msg (in case)
-				debugMode = false;
 			}
 		}
 		// Else, return an error and help message
